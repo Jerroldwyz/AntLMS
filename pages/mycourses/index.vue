217a386f313064f81b429e0326813c2ccc3bfde9
Jamie Murray <jamiebmurray25@gmail.com>
2023-08-15 20:13:20 +1000

    [JM] move to singular page to manage courses

---
+++ pages/mycourses/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+  import CourseCreationForm from "~~/components/forms/CourseCreationForm"
+
+  const createDialog = ref(false)
+</script>
+
+<template>
+  <v-dialog v-model="createDialog">
+    <CourseCreationForm />
+  </v-dialog>
+  <v-btn @click="createDialog = true">Create Course</v-btn>
+</template>
