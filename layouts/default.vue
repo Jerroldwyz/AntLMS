f4ed27d9d312fdf23d648cd2f351d135a4d4a643
jamochl <james.lim@jamochl.com>
2023-08-23 12:16:25 +1000

    [JL] add menu to profile picture

--- layouts/default.vue
+++ layouts/default.vue
@@ -15,24 +15,63 @@
                 <v-spacer></v-spacer>
 
                 <v-responsive max-width="160">
-                <v-text-field
-                    density="compact"
-                    flat
-                    hide-details
-                    label="Course Search"
-                    rounded="lg"
-                    variant="filled"
-                    single-line
-                ></v-text-field>
+                    <v-text-field
+                        density="compact"
+                        flat
+                        hide-details
+                        label="Course Search"
+                        rounded="lg"
+                        variant="filled"
+                        single-line
+                    ></v-text-field>
                 </v-responsive>
 
-                <v-avatar
-                class="ms-4 "
-                color="grey-darken-1"
-                size="48"
+                <v-menu
+                    min-width="200px"
+                    rounded
                 >
-                    <span class="text-h6">AN</span>
-                </v-avatar>
+                    <template v-slot:activator="{ props }">
+                    <v-btn
+                        icon
+                        v-bind="props"
+                    >
+                        <v-avatar
+                        class="ms-4"
+                        color="grey-darken-1"
+                        size="large"
+                        >
+                            <span class="text-h6">AN</span>
+                        </v-avatar>
+                    </v-btn>
+                    </template>
+                    <v-card>
+                        <v-card-text>
+                            <div class="mx-auto text-center">
+                                <v-avatar
+                                    color="grey-darken-1"
+                                >
+                                    <span class="text-h5">AN</span>
+                                </v-avatar>
+                                <h3>ANONYMOUS TODO</h3>
+                                <p class="text-caption mt-1">ANONYMOUS@TODO.COM</p>
+                                <v-divider class="my-3"></v-divider>
+                                <v-btn
+                                    rounded
+                                    variant="text"
+                                >
+                                    <NuxtLink to="/account" class="text-button text-decoration-none text-black">Account Settings</NuxtLink>
+                                </v-btn>
+                                <v-divider class="my-3"></v-divider>
+                                <v-btn
+                                    rounded
+                                    variant="text"
+                                >
+                                    <NuxtLink to="/logout" class="text-button text-decoration-none text-black">Logout</NuxtLink>
+                                </v-btn>
+                            </div>
+                        </v-card-text>
+                    </v-card>
+                </v-menu>
             </v-container>
         </v-app-bar>
 
@@ -61,5 +100,4 @@
 </script>
 
 <style>
-
 </style>
