7191416f122b9421b586f808c5a5d8e39cd4220d
jamochl <james.lim@jamochl.com>
2023-08-28 20:31:47 +1000

    [JL] Add template for managing courses

---
+++ pages/admin/manage-courses/index.vue
@@ -0,0 +1,83 @@
+<template>
+    <h1 class="mb-4">Admin Panel - Courses</h1>
+    <v-text-field
+        clearable
+        label="Search"
+        type="text"
+        variant="outlined"
+    >
+        <template v-slot:prepend>
+            <v-tooltip location="bottom">
+                <template v-slot:activator="{ props }">
+                    <v-icon v-bind="props" icon="mdi-help-circle-outline"></v-icon>
+                </template>
+                Filter courses
+            </v-tooltip>
+        </template>
+    </v-text-field>
+
+    <div>
+        <v-table
+            fixed-header
+        >
+            <thead>
+            <tr>
+                <th class="text-left">
+                Thumbnail
+                </th>
+                <th class="text-left">
+                Course Name
+                </th>
+                <th class="text-left">
+                Description
+                </th>
+                <th class="text-right">
+                Action
+                </th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr
+                v-for="n in 9"
+            >
+                <td>
+                    <v-avatar
+                        class="ma-3"
+                        size="64"
+                        rounded="lg"
+                    >
+                        <v-img src="https://cdn.vuetifyjs.com/images/cards/foster.jpg"></v-img>
+                    </v-avatar>
+                </td>
+                <td>TITLE</td>
+                <td>DESCRIPTION</td>
+                <td class="text-right">
+                    <v-btn
+                        class="ms-2"
+                        variant="outlined"
+                        size="small"
+                    >
+                        View
+                    </v-btn>
+
+                    <v-btn
+                        class="ms-2"
+                        variant="outlined"
+                        size="small"
+                    >
+                        Disable
+                    </v-btn>
+                    <v-btn
+                        class="ms-2"
+                        variant="outlined"
+                        size="small"
+                        color="red"
+                    >
+                        Delete
+                    </v-btn>
+                </td>
+            </tr>
+            </tbody>
+        </v-table>
+    </div>
+</template>
