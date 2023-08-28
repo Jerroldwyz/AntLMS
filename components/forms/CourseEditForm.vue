4bb24e79875a223623b2cdb6d664f0b9bd2e26ea
Jamie Murray <tunnockz@gmail.com>
2023-08-28 20:59:58 +1000

    [JM] Feature/edit courses (#22)
    * change CreateCourseBtn from v-btn to v-card
    
    * changed Course to use utility classes
    
    * courses navigate to page based on id
    
    * created the list users will use to manage course topics
    
    * basic layout complete

---
+++ components/forms/CourseEditForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-card>
+    <v-container>
+      <v-row>
+        <v-col class="d-flex justify-start align-center">
+          <h4 class="text-h4">Details</h4>
+        </v-col>
+        <v-col class="d-flex justify-end align-center">
+          <v-btn class="mb-2" icon="mdi-dots-vertical" flat></v-btn>
+        </v-col>
+      </v-row>
+      <v-divider class="mb-2"></v-divider>
+      <v-form>
+        <v-text-field label="Title"></v-text-field>
+        <v-select label="Tag(s)" multiple chips></v-select>
+        <v-file-input label="Thumbnail"></v-file-input>
+      </v-form>
+    </v-container>
+    <v-card color="grey-lighten-3">
+      <v-container>
+        <v-row justify="end">
+          <v-btn class="text-capitalize" variant="text" @click="$emit('close')"> Cancel </v-btn>
+          <v-btn class="text-capitalize bg-primary" @click="$emit('course-success'); $emit('close')">
+            Save Changes
+          </v-btn>
+        </v-row>
+      </v-container>
+    </v-card>
+  </v-card>
+</template>
--- pages/mycourses/index.vue
+++ pages/mycourses/index.vue
@@ -5,10 +5,12 @@
   const courseErrorAlert = ref(true)
   const courses = ref([
     { 
-      title: "Baking 101",
+      id: 0,
+      title: "Intro to Baking",
       img: "https://images.pexels.com/photos/9095/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
     },
     { 
+      id: 1,
       title: "Math 101",
       img: "https://media.istockphoto.com/id/1219382595/vector/math-equations-written-on-a-blackboard.webp?s=1024x1024&w=is&k=20&c=FuAlO8n7UyfykyqpZMhWpQD66wIJuIbgXG7ZQPRgoPk="
     }
@@ -37,7 +39,7 @@
     <v-container fluid>
       <v-row>
         <v-col cols="2" v-for="course in courses">
-          <Course :title="course.title" :img="course.img" />
+          <Course :id="course.id" :title="course.title" :img="course.img" />
         </v-col>
         <v-col cols="2">
           <CreateCourseBtn @click="createCourseDialog = true" />
