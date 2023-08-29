5850fd049935a77d13d2b96b86f3acabcdf38539
Sahil Shrestha <super.sahil.99@gmail.com>
2023-08-29 23:14:37 +1000

    final commit

--- server/transformers/questions.ts
+++ server/transformers/questions.ts
@@ -3,8 +3,8 @@ import { choiceTransformer } from "./choices"
 export const questionsTransformer = (question: any) => {
     return {
         id: question.id,
-        questionText: question.question_text,
-        explanation: question.explanation ?? "",
+        questionText: question.question_text ?? "Empty Field",
+        explanation: question.explanation ?? "Empty Field",
         choices: question.choices.map(choiceTransformer),
     }
 }
