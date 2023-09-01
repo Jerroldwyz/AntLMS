7ec3c76f54e91c620a434fd4bcbed1d53979f8ab
Sahil Shrestha <super.sahil.99@gmail.com>
2023-09-01 12:19:45 +1000

    Minor fixes to the enrollment

--- server/api/courses/enroll.post.ts
+++ server/api/courses/enroll.post.ts
@@ -1,7 +1,13 @@
+import { error } from "console"
 import { enrollUser } from "~~/server/db/enrollment"
 
 export default defineEventHandler(async (event) => {
   const body = await readBody(event)
-
-  return await enrollUser(parseInt(body.courseId), body.userId as string)
+  try {
+    return await enrollUser(parseInt(body.courseId), body.userId as string)
+  } catch (Exception) {
+    throw createError({
+      statusMessage: "The enrollment already exists",
+    })
+  }
 })
