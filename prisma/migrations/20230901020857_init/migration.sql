9a47899f5325838596c23a0c9c055a74caf9d332
jamochl <james.lim@jamochl.com>
2023-09-04 17:44:26 +1000

    [JL] add auth middleware back, remove prisma/migrations

--- prisma/migrations/20230901020857_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[course_id,user_id]` on the table `enrollments` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "enrollments_course_id_user_id_key" ON "enrollments"("course_id", "user_id");
