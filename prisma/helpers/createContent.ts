21407c9d67cf07cf6e5593633cdef76b5cc2d68a
jamochl <james.lim@jamochl.com>
2023-10-04 18:15:17 +1100

    [JL] change content seeding to html

--- prisma/helpers/createContent.ts
+++ prisma/helpers/createContent.ts
@@ -18,7 +18,11 @@ export const createMultipleContent = (topic: topics): content[] => {
     contents.push({
       id: faker.number.int(2147483647),
       title: faker.company.buzzPhrase(),
-      content: faker.lorem.paragraph(4),
+      content: `<h1>Heading 1</h1>
+<h2>Heading 2</h2>
+<h3>Heading 3</h3>
+<h4>Heading 4</h4>
+<p>${faker.lorem.paragraph(4)}</p>`,
       type: "TEXT",
       topic_id: topic.id,
       topic_position: faker.number.int(2147483647),
