163265b822bee83456ad5b6295086744771fd815
jamochl <james.lim@jamochl.com>
2023-10-08 21:44:22 +1100

    [JL] fix bug

--- server/api/courses/index.get.ts
+++ server/api/courses/index.get.ts
@@ -1,22 +1,22 @@
 import { object, string, InferType } from "yup"
 
 export default defineEventHandler(async (event) => {
-  try {
-    // Query params
-    const unvalidatedQueryParams = getQuery(event)
-    const queryParamsSchema = object({
-      status: string()
-        .optional()
-        .matches(/(all|enabled|disabled)/, { excludeEmptyString: true }),
-    })
-    type queryParamsType = InferType<typeof queryParamsSchema>
-    const queryParams = await validateAndParse<queryParamsType>({
-      schema: queryParamsSchema,
-      value: unvalidatedQueryParams,
-      msgOnError: "Bad query params",
-    })
-    const status = queryParams.status ?? "all"
+  // Query params
+  const unvalidatedQueryParams = getQuery(event)
+  const queryParamsSchema = object({
+    status: string()
+      .optional()
+      .matches(/(all|enabled|disabled)/, { excludeEmptyString: true }),
+  })
+  type queryParamsType = InferType<typeof queryParamsSchema>
+  const queryParams = await validateAndParse<queryParamsType>({
+    schema: queryParamsSchema,
+    value: unvalidatedQueryParams,
+    msgOnError: "Bad query params",
+  })
+  const status = queryParams.status ?? "all"
 
+  try {
     // all, enabled, disabled
     return await getAllCourses(status)
   } catch (e) {
