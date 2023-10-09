e4cb78d6413dd1a4e0f30ee7f179864196a7d5f0
jamochl <james.lim@jamochl.com>
2023-10-09 18:11:10 +1100

    [JL] refactor signup and signin

--- server/api/signin.post.ts
+++ server/api/signin.post.ts
@@ -1,17 +1,51 @@
+import { InferType, bool, object, string } from "yup"
+import { v4 as uuidv4 } from "uuid"
+import { createUser } from "~/server/utils/db/users"
+
 export default defineEventHandler(async (event) => {
-  const body = await readBody(event)
+  // Body params
+  const unvalidatedBody = await readBody(event)
+  const requestBodySchema = object({
+    uid: string().optional().uuid(),
+    name: string().required().min(1),
+    email: string().required().email(),
+    thumbnail: string().nullable().optional(),
+    contactDetails: object().optional(),
+    isAdmin: bool().optional(),
+    enabled: bool().optional(),
+  })
+  type requestBodyType = InferType<typeof requestBodySchema>
+  const body = await validateAndParse<requestBodyType>({
+    schema: requestBodySchema,
+    value: unvalidatedBody,
+    msgOnError: "Bad request body params",
+  })
 
-  const signInUser = await getUserById(body.uid)
-
-  if (!signInUser) {
-    return await createUser({
-      uid: body.uid,
-      name: body.name || "",
-      email: body.email,
-      thumbnail: body.thumbnail || "",
-      contact_details: body.contact_details || JSON.stringify({}),
-    })
+  if (body.uid === undefined) {
+    body.uid = uuidv4()
+  }
+  if (body.thumbnail === undefined) {
+    body.thumbnail = null
+  }
+  if (body.contactDetails === undefined) {
+    body.contactDetails = {}
+  }
+  if (body.isAdmin === undefined) {
+    body.isAdmin = false
+  }
+  if (body.enabled === undefined) {
+    body.enabled = true
   }
 
-  return signInUser
+  let signInUser
+  try {
+    signInUser = await getUserById(body.uid)
+    if (signInUser) {
+      return signInUser
+    } else {
+      return await createUser(camelCaseToUnderscore(body))
+    }
+  } catch (e) {
+    throw prismaErrorHandler(e)
+  }
 })
