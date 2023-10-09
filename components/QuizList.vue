f80c57e9f05cd2b4a9a9bf42fe218fe833c492cd
Sahil Shrestha <super.sahil.99@gmail.com>
2023-10-09 12:17:44 +1100

    added text editor to all the fields

--- components/QuizList.vue
+++ components/QuizList.vue
@@ -1,8 +1,9 @@
 <script setup>
 // TODO: change any to real type
-const props = defineProps(["quiz"])
+const props = defineProps(["quizId"])
 const emit = defineEmits(["clicked"])
-const quiz = ref(props.quiz)
+const quiz = await fetchUserQuiz(props.quizId)
+const dialog = ref(false)
 
 const deleteQuestion = async (id, index) => {
   try {
@@ -43,9 +44,42 @@ const deleteQuestion = async (id, index) => {
           <v-btn
             icon="mdi-delete"
             variant="flat"
-            @click="deleteQuestion(question.id, index)"
+            @click="dialog = true"
           />
         </v-col>
+        <v-dialog
+          v-model="dialog"
+          persistent
+          width="auto"
+        >
+          <v-card>
+            <v-card-title class="text-h5">
+              Are you sure you want to delete this quiz?
+            </v-card-title>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                color="green-darken-1"
+                variant="text"
+                @click="dialog = false"
+              >
+                No
+              </v-btn>
+              <v-btn
+                color="red-darken-1"
+                variant="elevated"
+                @click="
+                  () => {
+                    deleteQuestion(question.id, index)
+                    dialog = false
+                  }
+                "
+              >
+                Yes
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
       </v-row>
     </v-container>
   </v-card>
