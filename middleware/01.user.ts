b891531f4e13fffac63170319c559aa8c462504f
huuthien <kevinnguyen31415@gmail.com>
2023-10-12 23:10:47 +1100

    for the sake of usability testing i cheated

--- middleware/01.user.ts
+++ middleware/01.user.ts
@@ -1,6 +1,10 @@
 export default defineNuxtRouteMiddleware((to, from) => {
   const token = useFirebaseToken()
+  if (process.server) {
+    return
+  }
+
   if (!token.value) {
-    return navigateTo("/auth/login")
+    // return navigateTo("/auth/login")
   }
 })
